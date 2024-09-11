92373687499243073b60f56ce02d92968cb17738
hemasingh2000 <hemasingh486001@gmail.com>
2024-09-11 14:01:00 +0530

    new change

--- src/Routing.js
+++ src/Routing.js
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
-import {Route,Routes } from 'react-router-dom';
+import {Route,Routes,Link } from 'react-router-dom';
 import Home from './Home'
 import Electronic from './Electronics';
 import Jwellery from './Jwellery';
@@ -18,19 +18,19 @@ export default function Routing(){
       <Navbar bg="primary" data-bs-theme="dark">
         <Container>
          
-          <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/mobile">Mobile</Nav.Link>
-            <Nav.Link href="/laptop">Laptop</Nav.Link>
-            <Nav.Link href="/electronics">electronic</Nav.Link>
-            <Nav.Link href="/jewelery">Jwellery</Nav.Link>
-            <Nav.Link href="/mensclothing">Men clothes</Nav.Link>
-            <Nav.Link href="/womensclothing">Women clothes</Nav.Link>
+        
+            <Link to="/">Home</Link>
+            <Link to="/mobile">Mobile</Link>
+            <Link to="/laptop">Laptop</Link>
+            <Link to="/electronics">electronic</Link>
+            <Link to="/jewelery">Jwellery</Link>
+            <Link to="/mensclothing">Men clothes</Link>
+            <Link to="/womensclothing">Women clothes</Link>
 
 
-            <Nav.Link> <button>login</button></Nav.Link>
-            <Nav.Link> <button>Registration</button></Nav.Link>
-          </Nav>
+            <Link> <button>login</button></Link>
+            <Link> <button>Registration</button></Link>
+          
         </Container>
       </Navbar>
 <Routes>
@@ -40,10 +40,6 @@ export default function Routing(){
   <Route path='/jewelery' Component={Jwellery}></Route>
   <Route path='/mensclothing' Component={Men}></Route>
   <Route path='/womensclothing' Component={Women}></Route>
-  
-  
- 
-
 </Routes>
     </>
    
